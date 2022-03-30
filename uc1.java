fba4a32d2f7ca4efa3020be516529a2c98520fea
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-30 18:06:02 +0530

    Creating a simple linkedist

---
+++ uc1.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class uc1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to LinkedList program");
+        
+        List<Integer> list = new LinkedList<>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println(list);
+    }
+}
--- welcome.java
+++
@@ -1,7 +0,0 @@
-package linkedlist;
-
-public class welcome {
-    public static void main(String[] args) {
-        System.out.println("Welcome to LinkedList program");
-    }
-}
